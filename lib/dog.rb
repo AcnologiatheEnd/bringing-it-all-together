1b26e358b456eb16aa7e710d3748e6cc0d205170
Winston  Chung <18.chung.winston@ctkprep.org>
2019-11-21 19:00:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -83,7 +83,7 @@ attr_reader :id
   end
   
   def self.find_or_create_by(name, breed)
-    dog = DB[:conn].execute("SELECT * FROM dogs")
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? ")
   end
 
 
